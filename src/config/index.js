fa00a1c7c818cf75d83ddd3c815956fae7bb9f78
Chelsea Niemeier <32147958+chelseaniemeier@users.noreply.github.com>
2018-03-22 19:12:44 -0400

    update

--- src/config/index.js
+++ src/config/index.js
@@ -2,25 +2,13 @@
 
 
 // I used dotenv to password protect my mLab database password. In the README file, require anyone that clones project to create a .env file of their own, with their own database host, username, and password. My .env file is in my .gitignore. I will provide my password to Brian or Code Louisville rep so they can see my data on the localhost:3030 for the purpose of this project.
-// module.exports = {
-//     appName: 'Anime Recommendations',
-//     port: 3030,
-//     db: {
-//       username: process.env.DB_USERNAME,
-//       password: process.env.DB_PASSWORD,
-//       host: process.env.DB_HOST,
-//       dbName: 'anime-r',
-//     }
-//   };
-
-  // I used dotenv to password protect my mLab database password. In the README file, require anyone that clones project to create a .env file of their own, with their own database host, username, and password. My .env file is in my .gitignore. I will provide my password to Brian or Code Louisville rep so they can see my data on the localhost:3030 for the purpose of this project.
 module.exports = {
-  appName: 'Anime Recommendations',
-  port: 3030,
-  db: {
-    username: 'cniemeier',
-    password: 'arigato1',
-    host: 'ds249428.mlab.com:49428',
-    dbName: 'anime-r',
-  }
-};
+    appName: 'Anime Recommendations',
+    port: 3030,
+    db: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      dbName: 'anime-r',
+    }
+  };
